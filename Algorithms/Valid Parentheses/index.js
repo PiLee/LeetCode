d3b5e3ba0ca4629aeca6de98e5d3e952657bb13f
pilee <pilee0827@163.com>
2016-07-09 19:43:17 +0800

    update Valid Parentheses

--- Algorithms/Valid Parentheses/index.js
+++ Algorithms/Valid Parentheses/index.js
@@ -4,24 +4,28 @@
  */
 var isValid = function(s) {
 	var len=s.length;
-	if(len<2 || len%2!==0) return false;
     var preset={};
+    var cache=[];
     preset['[']=']';
     preset['{']='}';
     preset['(']=')';
-    preset[')']='(';
-    preset['}']='{';
-    preset[']']='[';
-    for(var i=0;i<len/2;i++){
-    	if(!preset[s[i]]) return false;
-    	if(s[i+1]===preset[s[i]]){
-    		i++;
-    	}else if(s[i+1]===preset[s[len-i-2]]){
-    		i++;
-    	}else{
-    		return false
-    	}
-    	return true;
+    if(len<2 || len%2!==0 || !preset[s[0]]) return false;
+    for(var i=0;i<len;i++){
+        var curItem=s[i];
+        if(cache.length===0){
+            cache.push(curItem)
+        }else{
+            if(curItem === preset[cache[cache.length-1]]){
+                cache.pop();
+            }else{
+                cache.push(curItem);
+            }
+        }
+    }
+    if(cache.length===0){
+        return true
+    }else{
+        return false;
     }
 
 };
@@ -30,5 +34,9 @@ var s='[]{}()'
 console.log(isValid(s));
 var s='['
 console.log(isValid(s));
+var s='(('
+console.log(isValid(s));
 var s='([])'
+console.log(isValid(s));
+var s='([(){}]{})'
 console.log(isValid(s));
